8bdc4ed18881b3186b515a51331ada19ed974810
xofyerg <xofyerg1@gmail.com>
2022-05-09 00:40:49 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import Layout from "./hoc/Layout/Layout";
 import { Navigate, Route, Routes } from "react-router-dom";
 import QuizCreator from "./containers/QuizCreator/QuizCreator";
@@ -5,12 +6,20 @@ import QuizList from "./containers/QuizList/QuizList";
 import Auth from "./containers/Auth/Auth";
 import Quiz from "./containers/Quiz/Quiz";
 import Home from "./components/Home/Home";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Logout from "./components/Logout/Logout";
+import { autoLogin } from "./redux/actions/auth";
 
 // yarn upgrade-interactive --latest
 // yarn add -D [package]
-const App = (props) => {
+const App = () => {
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(autoLogin());
+  }, []);
+
   let routes = (
     <Routes>
       <Route path="/auth" element={<Auth />} />
@@ -18,19 +27,19 @@ const App = (props) => {
       <Route path="/quiz-list" element={<QuizList />} />
       <Route path="/" element={<Home />} />
       {/*<Route path="/" element={} />*/}
-      <Navigate to="/" />
+      {/*<Navigate to="/" />*/}
     </Routes>
   );
 
-  if (props.isAuth) {
+  if (token) {
     routes = (
       <Routes>
-        <Route path="/quiz/:id" element={<Quiz />} />
         <Route path="/quiz-creator" element={<QuizCreator />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
         <Route path="/quiz-list" element={<QuizList />} />
-        <Route path="/" element={<Home />} />
         <Route path="/logout" element={<Logout />} />
-        <Navigate to="/" />
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Navigate to="/" />} />
       </Routes>
     );
   }
@@ -38,10 +47,52 @@ const App = (props) => {
   return <Layout>{routes}</Layout>;
 };
 
-function mapStateToProps(state) {
-  return {
-    isAuth: !!state.auth.token,
-  };
-}
+export default App;
 
-export default connect(mapStateToProps, null)(App);
+// class App extends React.Component {
+//   componentDidMount() {
+//     this.props.autoLogin();
+//   }
+//
+//   render() {
+//     let routes = (
+//       <Routes>
+//         <Route path="/auth" element={<Auth />} />
+//         <Route path="/quiz/:id" element={<Quiz />} />
+//         <Route path="/quiz-list" element={<QuizList />} />
+//         <Route path="/" element={<Home />} />
+//         {/*<Route path="/" element={} />*/}
+//         {/*<Navigate to="/" />*/}
+//       </Routes>
+//     );
+//
+//     if (this.props.isAuth) {
+//       routes = (
+//         <Routes>
+//           <Route path="/quiz-creator" element={<QuizCreator />} />
+//           <Route path="/quiz/:id" element={<Quiz />} />
+//           <Route path="/quiz-list" element={<QuizList />} />
+//           <Route path="/logout" element={<Logout />} />
+//           <Route path="/" element={<Home />} />
+//           <Route path="/auth" element={<Navigate to="/" />} />
+//         </Routes>
+//       );
+//     }
+//
+//     return <Layout>{routes}</Layout>;
+//   }
+// }
+//
+// function mapStateToProps(state) {
+//   return {
+//     isAuth: !!state.auth.token,
+//   };
+// }
+//
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     autoLogin: () => dispatch(autoLogin()),
+//   };
+// }
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { logout } from "../../redux/actions/auth";
 
 class Logout extends React.Component {
